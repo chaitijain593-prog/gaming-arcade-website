9b1474b27351ce2d836faaa9b6f71fc4bb6fd7a9
chakshu patel <chakshupatel20112007@gmail.com>
2026-05-02 13:25:57 +0530

    added local storage to wam

--- landing_page/whack_a_mole/mole.js
+++ landing_page/whack_a_mole/mole.js
@@ -2,9 +2,12 @@ let currMoleTile;
 let currPlantTile;
 let score = 0;
 let gameOver = false;
+let highScore = localStorage.getItem("high-score") || 0;
 
 window.onload = function() {
     setGame();
+   
+    document.getElementById("high-score").innerText = "High Score: " + highScore;
 }
 
 function setGame() {
@@ -24,51 +27,55 @@ function getRandomTile() {
 }
 
 function setMole() {
-    if (gameOver) {
-        return;
-    }
-    if (currMoleTile) {
-        currMoleTile.innerHTML = "";
-    }
+    if (gameOver) return;
+    if (currMoleTile) currMoleTile.innerHTML = "";
+    
     let mole = document.createElement("img");
     mole.src = "./monty-mole.png";
 
     let num = getRandomTile();
-    if (currPlantTile && currPlantTile.id == num) {
-        return;
-    }
+    if (currPlantTile && currPlantTile.id == num) return;
+    
     currMoleTile = document.getElementById(num);
     currMoleTile.appendChild(mole);
 }
 
 function setPlant() {
-    if (gameOver) {
-        return;
-    }
-    if (currPlantTile) {
-        currPlantTile.innerHTML = "";
-    }
+    if (gameOver) return;
+    if (currPlantTile) currPlantTile.innerHTML = "";
+    
     let plant = document.createElement("img");
     plant.src = "./piranha-plant.png";
 
     let num = getRandomTile();
-    if (currMoleTile && currMoleTile.id == num) {
-        return;
-    }
+    if (currMoleTile && currMoleTile.id == num) return;
+    
     currPlantTile = document.getElementById(num);
     currPlantTile.appendChild(plant);
 }
 
 function selectTile() {
-    if (gameOver) {
-        return;
-    }
+    if (gameOver) return;
+
     if (this == currMoleTile) {
         score += 10;
-        document.getElementById("score").innerText = score.toString(); //update score html
+        document.getElementById("score").innerText = "Score: " + score;
+
+     
+        if (score > highScore) {
+            highScore = score;
+            localStorage.setItem("high-score", highScore);
+            document.getElementById("high-score").innerText = "High Score: " + highScore;
+        }
     }
     else if (this == currPlantTile) {
-        document.getElementById("score").innerText = "GAME OVER: " + score.toString(); //update score html
+        document.getElementById("score").innerText = "GAME OVER: " + score;
         gameOver = true;
+
+        
+        let sessionGames = parseInt(sessionStorage.getItem("games-played") || 0);
+        sessionStorage.setItem("games-played", sessionGames + 1);
+        
+        console.log("Session Game Count: ", sessionStorage.getItem("games-played"));
     }
 }
